b5e98ad75dab0d2135032438b8ec240430ac1a3f
Sharadsinh Vadher <sharadsinh@yahoo.com>
2015-12-20 22:39:08 -0500

    Login and Register screen changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,42 +2,80 @@
 
 @section('content')
 
-    <p>Already have an account? <a href='/login'>Login here...</a></p>
 
-    <h1>Signup</h1>
+<div class="container">
 
-    @include('errors')
+    <div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        @include('errors')
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="panel-title">Sign Up</div>
+                {{-- <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div> --}}
+            </div>
+            <div class="panel-body" >
+                <form id="signupform" class="form-horizontal" role="form" method='POST' action='/register'>
+                    {!! csrf_field() !!}
 
-    <form method='POST' action='/register'>
-        {!! csrf_field() !!}
+                    <div id="signupalert" style="display:none" class="alert alert-danger">
+                        <p>Error:</p>
+                        <span></span>
+                    </div>
 
-        <div class='form-group'>
-            <label for='name'>First Name</label>
-            <input type='text' name='firstname' id='firstname' value='{{ old('firstname') }}'>
+                    <div class="form-group">
+                        <label for="firstname" class="col-md-3 control-label">First Name</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="firstname" placeholder="First Name" value='{{ old('firstname') }}'>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="lastname" class="col-md-3 control-label">Last Name</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="lastname" placeholder="Last Name" value='{{ old('lastname') }}'>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="col-md-3 control-label">Email</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="email" placeholder="Email Address" value='{{ old('email') }}'>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="col-md-3 control-label">Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class='form-group'>
+                        <label for='password_confirmation' class="col-md-3 control-label">Confirm Password</label>
+                        <div class="col-md-9">
+                            <input type='password' class="form-control" name='password_confirmation' id='password_confirmation' placeholder="Retype Password">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <!-- Button -->
+                        <div class="col-md-offset-3 col-md-9">
+                            <button id="btn-signup" type="submit" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Sign Up</button>
+
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <div class="col-md-12 control">
+                            <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                                Already have an account?
+                                <a href="/login"> {{-- onClick="$('#loginbox').hide(); $('#signupbox').show()"> --}}
+                                    Login Here
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <div class='form-group'>
-            <label for='name'>Last Name</label>
-            <input type='text' name='lastname' id='lastname' value='{{ old('lastname') }}'>
-        </div>
-
-        <div class='form-group'>
-            <label for='email'>Email</label>
-            <input type='text' name='email' id='email' value='{{ old('email') }}'>
-        </div>
-
-        <div class='form-group'>
-            <label for='password'>Password</label>
-            <input type='password' name='password' id='password'>
-        </div>
-
-        <div class='form-group'>
-            <label for='password_confirmation'>Confirm Password</label>
-            <input type='password' name='password_confirmation' id='password_confirmation'>
-        </div>
-
-        <button type='submit' class='btn btn-primary'>Register</button>
-
-    </form>
-
+    </div>
+</div>
 @stop

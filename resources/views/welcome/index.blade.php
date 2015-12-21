5fdeccc8ef388ed88b7aa2cb381fb7616b157827
Sharad Vadher <svadher@ptc.com>
2015-12-21 13:23:59 -0500

    Work in proggress for front page

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -4,7 +4,24 @@
     Welcome
 @stop
 
+@section('head')
+
+@stop
+
 @section('content')
+    <div class="container">
 
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Grocery List App</h1>
+                <p class="lead">A Solution to maintain and share grocery list</p>
+                <ul class="list-unstyled">
+                    <li>Bootstrap v3.3.6</li>
+                    <li>jQuery v1.11.1</li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
 
+    </div>
 @stop
